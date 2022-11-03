c47c9cc132d766541251b9e7343354bb201ac127
JoaquinAyG <joaquin.ayllongar@gmail.com>
2022-11-03 10:42:36 +0100

    init repo

---
+++ app/build/generated/source/buildConfig/debug/io/material/materialthemebuilder/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.material.materialthemebuilder;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "io.material.materialthemebuilder";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
